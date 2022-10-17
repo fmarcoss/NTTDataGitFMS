24c2198d97c05844e91fd12d734a5add084294c6
fmarcoss <fmarcoss@SVQ-BJ73WT2.usersad.everis.int>
2022-10-17 17:51:33 +0200

    Second commit

--- NTTDataGitFMS/App.java
+++ NTTDataGitFMS/App.java
@@ -15,7 +15,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hola Mundo");
+		System.out.println("Hola , Soy Francisco Marcos Sánchez");
 
 	}
 
