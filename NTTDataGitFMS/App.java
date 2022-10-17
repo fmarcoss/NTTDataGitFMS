f103e6dda8901876b48539845a43df58b5cea1f8
fmarcoss <fmarcoss@SVQ-BJ73WT2.usersad.everis.int>
2022-10-17 17:51:01 +0200

    Initial commit

---
+++ NTTDataGitFMS/App.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package NTTDataGitFMS;
+
+/**
+ * @author fmarcoss
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola Mundo");
+
+	}
+
+}
